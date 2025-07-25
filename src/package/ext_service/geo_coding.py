15af9bdc0fd600730549eab07db28b04e05e5ef7
Adam <34325950+AdTaylorT@users.noreply.github.com>
2025-07-25 19:55:42 -0400

    Issue #8 additional locations (#16)
    * clean up some of the duplicate tk stuff,
    
    handle errors better,
    add dynamically
    delete dynamically
    
    * use a global to track the number of separators ...
    
    it's a dumb solution but until i make this module more OOP it's fine
    
    * address codeQL errors
    
    * updates and fixes from PR

--- src/package/ext_service/geo_coding.py
+++ src/package/ext_service/geo_coding.py
@@ -1,3 +1,6 @@
+from typing import Any
+from numpy import isnan
+
 import pgeocode as pg
 
 from errors.not_found_error import NotFoundError
@@ -8,44 +11,48 @@ class GeoCode():
     def __init__(self, country_code='US'):
         self.nomi = pg.Nominatim(country_code)
 
-    def fuzzy_name_lookup(self, city, state='Virginia'):
-        #city = city.capitalize().strip()
-        #state = state.lower().strip()
+    def fuzzy_name_lookup(self, city: str, state='Virginia') -> tuple[Any, Any]:
+        """
+        find lat/long based on city name, with a filter on state, default to Virginia
+
+        returns a tuple of lat and lon
+        """
+        city = city.capitalize().strip()
 
         query = self.nomi.query_location(name=city, fuzzy_threshold=70)
         # Keep rows where state_name is 'Virginia'
         if len(state) == 2:
+            state = state.upper().strip()
             query = query[query['state_code'] == state]
         else:
+            state = state.capitalize().strip()
             query = query[query['state_name'] == state]
 
         if query.empty:
             print(f"City {city} not found in State {state}.")
             raise NotFoundError("City not found.")
 
-        data = { 'lat': query['latitude'].values[0], 'lon': query['longitude'].values[0] }
-        return data
+        return (query['latitude'].values[0], query['longitude'].values[0])
 
-    def zipcode_lookup(self, zipcode):
+    def zipcode_lookup(self, zipcode: str) -> tuple[Any, Any]:
+        """
+        get the lat and long for a given zipcode
+
+        returns a tuple of lat and lon
+        """
         #zipcode = zipcode.strip()
         if not zipcode.isdigit() or len(zipcode) != 5:
             print(f"Invalid zip code: {zipcode}. It should be a 5-digit number.")
             raise ValueError("Zip code must be a 5-digit number.")
 
-        q2 = self.nomi.query_postal_code(codes=zipcode)
-        q2.to_dict()
-
-        if q2.empty:
+        query = self.nomi.query_postal_code(codes=zipcode)
+        if query is None or query.empty or isnan(query['latitude']):
             print(f"Zip code {zipcode} not found.")
             raise NotFoundError("Zip code not found.")
 
-        data2 = { 'lat': q2['latitude'], 'lon': q2['longitude'] }
-        return data2
+        return (query['latitude'], query['longitude'])
 
 if __name__ == "__main__":
     geo = GeoCode()
     lu1 = geo.fuzzy_name_lookup('Vienna')
     lu2 = geo.zipcode_lookup('22314')
-
-    print(f"Fuzzy name lookup: {lu1}")
-    print(f"Zip code lookup: {lu2}")
